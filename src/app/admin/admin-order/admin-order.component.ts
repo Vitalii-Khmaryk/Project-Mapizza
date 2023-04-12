617f9754577fe82a47e225ecb92e9656709b1ec1
Vitalii-Khmaryk <vitalikhmaryks@gmail.com>
2023-04-12 21:10:05 +0300

    fix order.component.ts

--- src/app/admin/admin-order/admin-order.component.ts
+++ src/app/admin/admin-order/admin-order.component.ts
@@ -1,23 +1,35 @@
 import { Component } from '@angular/core';
-
+import {
+  Firestore,
+  CollectionReference,
+  collectionData,
+} from '@angular/fire/firestore';
+import {
+  collection,
+  DocumentData,
+} from '@firebase/firestore';
 @Component({
   selector: 'app-admin-order',
   templateUrl: './admin-order.component.html',
-  styleUrls: ['./admin-order.component.scss']
+  styleUrls: ['./admin-order.component.scss'],
 })
 export class AdminOrderComponent {
-  public orderAdmin!:any[];
-ngOnInit(): void {
-  this.loadOrder();
-}
-
-loadOrder(){
-  let ordersArray = JSON.parse(localStorage.getItem('orders') as string);
-  if (!ordersArray) {
-    return;
-  } else {
-    this.orderAdmin=ordersArray;
+  public orderAdmin!: any[];
+  private orderCollection!: CollectionReference<DocumentData>;
+  constructor(private afs: Firestore) {
+    this.orderCollection = collection(this.afs, 'orders');
   }
-}
 
+  getAllFirebase() {
+    return collectionData(this.orderCollection, { idField: 'id' });
+  }
+  ngOnInit(): void {
+    this.loadOrder();
+  }
+
+  loadOrder() {
+    this.getAllFirebase().subscribe((data) => {
+      this.orderAdmin = data as Array<any>;
+    });
+  }
 }
